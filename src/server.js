460a257972e3fa17827b3a3ffb9720dbc315ed64
Rodrigo Molter <rodrigo.molter@gmail.com>
2024-03-29 14:52:06 -0300

    refactor: removing unused code

--- src/server.js
+++ src/server.js
@@ -1,19 +1,8 @@
 import { fastify } from "fastify"
 import { DatabaseMemory } from "./database-memory.js"
-import { fastifySwagger } from "@fastify/swagger"
-import { fastifySwaggerUi } from "@fastify/swagger-ui"
 
 const server = fastify()
 const db = new DatabaseMemory()
-
-await server.register(fastifySwagger)
-await server.register(fastifySwaggerUi, {
-    routePrefix: '/docs',
-    uiConfig: {
-      docExpansion: 'full',
-      deepLinking: false
-    }
-  })
 
 server.get('/videos', (req, res) => {
     const search = req.query.search
